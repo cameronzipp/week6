947c7b47728d068a859a419a01ae87dca64da003
Cameron Zipp <Zipp.Cameron@student.greenriver.edu>
2022-02-08 16:16:45 -0500

    Creating files

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-//turn on output buffering
-ob_start();
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
-//start session
-session_start();
-var_dump($_SESSION);
 
 //require autoload file
 require_once('vendor/autoload.php');
@@ -21,17 +15,5 @@ $f3->route('GET /', function() {
     echo $view->render('views/info.html');
 });
 
-//Define route for the summary
-$f3->route('GET /summary', function() {
-    $view = new Template();
-    echo $view->render('views/summary.html');
-
-    //clear session data
-    session_destroy();
-});
-
 //run fat-free
 $f3->run();
-
-//send output to the browser
-ob_flush();
