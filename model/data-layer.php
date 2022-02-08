542ce4b4ad5a44ecd9f20eaffcb4040481bf38dc
Cameron Zipp <Zipp.Cameron@student.greenriver.edu>
2022-02-08 17:17:20 -0500

    Adding Model path and file

---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+function getDesserts() {
+    $desserts = array('chocolate'=>'Chocolate Mousse','vanilla'=>'Vanilla Custard',
+        'strawberry'=>'Strawberry Shortcake');
+    return $desserts;
+}
+
+function getColors() {
+    $colors = array('Black', 'Green', "Red", "Blue");
+    return $colors;
+}
