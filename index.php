06b64997a1214f634a5a497cf0d2ba6310040226
Cameron Zipp <Zipp.Cameron@student.greenriver.edu>
2022-02-08 17:17:47 -0500

    Adding more templating

--- index.php
+++ index.php
@@ -5,12 +5,28 @@ error_reporting(E_ALL);
 
 //require autoload file
 require_once('vendor/autoload.php');
+require('model/data-layer.php');
 
 //create instance of the base class
 $f3 = Base::instance();
 
 //define default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+
+    //Save data to the hive
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+    $f3->set('color', 'Black');
+
+    $fruits = array('apple', 'banana', 'orange');
+    $f3->set('fruits', $fruits);
+
+    $f3->set('desserts', getDesserts());
+
+    $f3->set('colors', getColors());
+
+
     $view = new Template();
     echo $view->render('views/info.html');
 });
